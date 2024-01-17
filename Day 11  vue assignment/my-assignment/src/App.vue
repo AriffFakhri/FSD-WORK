e110ad04dcd46936fa4e37462f9a05b43bf2eb4b
ariff <arifffakhri@gmail.com>
2024-01-17 15:30:27 +0800

    day 11 assignment

---
+++ Day 11  vue assignment/my-assignment/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <AssignmentOne></AssignmentOne>
+  
+ 
+</template>
+
+<script>
+import AssignmentOne from './components/AssignmentOne.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    AssignmentOne,
+   
+  }
+}
+</script>
+
+<style>
+ #app {
+  text-align: center;
+  padding: 20px;
+  background-color: black;
+  color: white;
+}
+.Counter button{
+  margin-left: 20px;
+  align-items: center;
+  background-color: aqua;
+  font-size: 30px;
+  color: white;
+  padding: 10px;
+
+}
+</style>
